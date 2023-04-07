d3ca45a25317df3240a142408e942f77d3b3785d
王勇 <wy19945372694@163.com>
2023-04-07 19:47:02 +0800

    update

--- handleHTML.js
+++ handleHTML.js
@@ -6,7 +6,11 @@ const regStyle = /<style>[\s\S]*<\/style>/
 
 const regScript = /<script>[\s\S]*<\/script>/
 
-fs.readFile(path.join(__dirname,"index.html"),"utf8",(err,dataSrc) => {
-    err ? console.log("读取文件失败！"+err.message) : console.log("读取文件成功！" + dataSrc);
+fs.readFile(path.join(__dirname, "index.html"), "utf8", (err, dataSrc) => {
+    err ? console.log("读取文件失败！" + err.message) : console.log("读取文件成功！", resolveCSS(dataSrc))
 })
 
+const resolveCSS = e => {
+    const x = regStyle.exec(e)
+    console.log(x[0].replace("<style>", "").replace("</style>", ""))
+}
