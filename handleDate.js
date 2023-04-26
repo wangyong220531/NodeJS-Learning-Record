bbb2eba4ce18b3056dca4dfc5e9bc201fbe126f8
王勇 <wy19945372694@163.com>
2023-04-26 11:24:49 +0800

    update

---
+++ handleDate.js
@@ -0,0 +1,18 @@
+const handleDate = e => {
+    const dt = new Date(e)
+    const Y = dt.getFullYear()
+    const M = padZero(dt.getMonth())
+    const D = padZero(dt.getDay())
+    const h = padZero(dt.getHours())
+    const m = padZero(dt.getMinutes())
+    const s = padZero(dt.getSeconds())
+    return `${Y}-${M}-${D} ${h}:${m}:${s}`
+}
+
+const padZero = e => {
+    return e > 9 ? e : "0" + e
+}
+
+module.exports = {
+    handleDate
+}
--- test.js
+++ test.js
@@ -2,10 +2,16 @@
 
 // console.log(cstm);
 
-const cstm = require("./模块作用域")
+// const cstm = require("./模块作用域")
 
-console.log(cstm);
+// console.log(cstm);
 
 // console.log(module);
 
 // console.log( exports === module.exports);
+
+const HandleDate = require("./handleDate")
+
+const time = new Date()
+
+console.log(HandleDate.handleDate(time))
