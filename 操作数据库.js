a70d1f77ef199b45cacde3595ba4115fdbb7e61d
王勇 <wy19945372694@163.com>
2023-05-08 09:36:19 +0800

    update

--- 操作数据库.js
+++ 操作数据库.js
@@ -4,20 +4,20 @@ const db = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
     password: "220531",
-    database: "mydb00"
+    database: "prisma"
 })
 
 // db.query("SELECT * FROM users", (err, res) => {
 //     err ? console.log(err.message) : console.log(res)
 // })
 
-// const insertData = { name: "蛮王", age: 222, sex: 0 }
+const insertData = { name: "蛮王", age: 222, status: 0 }
 
-// const sqlStr = "INSERT INTO users (name,age,sex) VALUES (?,?,?)"
+const sqlStr = "INSERT INTO users (name,age,status) VALUES (?,?,?)"
 
-// db.query(sqlStr, [insertData.name, insertData.age, insertData.sex], (err, res) => {
-//     err ? console.log(err.message) : res.affectedRows === 1 ? console.log("插入成功！") : console.log("插入失败！")
-// })
+db.query(sqlStr, [insertData.name, insertData.age, insertData.status], (err, res) => {
+    err ? console.log(err.message) : res.affectedRows === 1 ? console.log("插入成功！") : console.log("插入失败！")
+})
 
 // const insertData = { name: "蛮王", age: 222, sex: 0 }
 
@@ -35,8 +35,8 @@ const db = mysql.createPool({
 //     err ? console.log(err.message) : res.affectedRows === 8 ? console.log("更新成功！") : console.log("更新失败！")
 // })
 
-const sqlStr = "DELETE FROM users WHERE id=?"
+// const sqlStr = "DELETE FROM users WHERE id=?"
 
-db.query(sqlStr, 13, (err, res) => {
-    err ? console.log(err.message) : res.affectedRows === 1 ? console.log("删除成功！") : console.log("删除失败！")
-})
+// db.query(sqlStr, 13, (err, res) => {
+//     err ? console.log(err.message) : res.affectedRows === 1 ? console.log("删除成功！") : console.log("删除失败！")
+// })
